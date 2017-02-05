68018f75c2241ca664b32776e45499d4f6e60108
Mottaqui Karim <taqqui.karim@gmail.com>
2017-02-05 13:12:56 -0500

    added search button functionality, UI only

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,20 @@
 (function() { // protect the lemmings!
 
+
+	const button = document.querySelector('.js-search');
+	const input = document.querySelector('.js-gif-input');
+
+	button.addEventListener('click', (e) => {
+		const searchTerm = input.value;
+		input.value = "";
+
+		if (searchTerm.trim() === "") {
+			alert('Please input a value!')
+			return;
+		}
+
+		input.setAttribute('disabled', 'disabled');
+		button.setAttribute('disabled', 'disabled');
+	});
+
 })();
